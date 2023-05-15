33782172e607a835ed561c72c48125be59b9a190
John Josiah <John.J@bullseye.local>
2023-05-15 13:26:52 +0100

    my work

---
+++ sell_easy/core/models.py
@@ -0,0 +1,52 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.db.models import Q 
+
+# Create your models here.
+# modules 
+# models
+
+class Store(models.Model):
+    owner = models.OneToOneField(User, on_delete=models.CASCADE)
+    tagline = models.CharField(max_length=200)
+    name = models.CharField(max_length=100)
+    def __str__(self) ->str:
+        return self.name
+
+    
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self) ->str:
+        return self.name
+
+class Products(models.Model):
+    # owner
+    store = models.ForeignKey(Store, on_delete=models.DO_NOTHING, related_name="products", null=True)
+    category = models.ForeignKey(Category, on_delete=models.DO_NOTHING, related_name="products", null=True)
+    name = models.CharField(max_length=100)
+    desc = models.TextField()
+    price = models.DecimalField(decimal_places=2,max_digits=10)
+    is_available = models.BooleanField(default=True)
+    date_created = models.DateField(auto_now_add=True )
+    rating = models.IntegerField()
+
+    def discount(self, discount=20):
+        self.price = self.price * (discount/100)
+        return self.price
+    
+
+    def __str__(self) -> str:
+        return f"{self.name},{self.desc} {self.price}"
+ 
+ # decribes the behaviout of your modeln
+class Meta:
+    ordering = {'name', '-price',}
+    verbose_name = 'products'
+    constraints = [models.CheckConstraint(check=Q('price__gt=12000'))]
+    
+
+
+
+
+
---
+++ sell_easy/core/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+
+# Create your views here.
+def do_nothing(request):
+    pass
